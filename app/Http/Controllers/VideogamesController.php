02f044c41b458a471303d904a3679be39e7a8eff
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-05-11 19:23:53 +0200

    migrate

---
+++ app/Http/Controllers/VideogamesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\videogames;
+use Illuminate\Http\Request;
+
+class VideogamesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\videogames  $videogames
+     * @return \Illuminate\Http\Response
+     */
+    public function show(videogames $videogames)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\videogames  $videogames
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(videogames $videogames)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\videogames  $videogames
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, videogames $videogames)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\videogames  $videogames
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(videogames $videogames)
+    {
+        //
+    }
+}
---
+++ database/factories/VideogamesFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\videogames;
+use Faker\Generator as Faker;
+
+$factory->define(videogames::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
